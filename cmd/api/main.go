7568c466dcbc5181ecaa2904a1ce95b1a365e76e
Soul-Remix <soulremix-dev@pm.me>
2023-12-21 18:50:47 +0200

    Add sql db connection and env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
 type config struct {
-	port int
+	port string
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns string
+		maxIdleConns string
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -21,14 +33,34 @@ type application struct {
 	logger *log.Logger
 }
 
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.port, "port", getEnv("PORT", "4000"), "API server port")
+	flag.StringVar(&cfg.env, "env", getEnv("ENVIRONMENT", "development"), "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.maxOpenConns, "db-max-open-conns", getEnv("DB_MAX_IDLE_TIME", "25"), "PostgreSQL max open connections")
+	flag.StringVar(&cfg.db.maxIdleConns, "db-max-idle-conns", getEnv("DB_MAX_IDLE_TIME", "25"), "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", getEnv("DB_MAX_IDLE_TIME", "15m"), "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -36,7 +68,7 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Addr:         fmt.Sprintf(":%s", cfg.port),
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
@@ -44,6 +76,43 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func getEnv(env, value string) string {
+	if v := os.Getenv(env); v != "" {
+		return v
+	}
+	return value
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	maxOpenCon, _ := strconv.Atoi(cfg.db.maxOpenConns)
+	maxIdleCon, _ := strconv.Atoi(cfg.db.maxIdleConns)
+
+	db.SetMaxOpenConns(maxOpenCon)
+	db.SetMaxIdleConns(maxIdleCon)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
