a79998b0b71edea5f9987c981818504112b7fa97
Soul-Remix <soulremix-dev@pm.me>
2024-02-19 20:25:21 +0300

    Add authenticate middleware

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -56,3 +56,8 @@ func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *htt
 	message := "invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
